45f7116f8007b4f234dfb53e943e8fb9bbeb6ee5
otake <otake.hiroyo@gmail.com>
2016-12-05 00:59:11 +0900

    ファイルにデータを書き込み

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php
+
+$fileName = "bbs.dat";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	// var_dump($_POST);
 	$title = $_POST['title'];
 	$impression = $_POST['impression'];
 	$name = $_POST['name'];
+
+	$data = $title . "\t" . $impression . "\t" . $name . "\n";
+
+	$fp = fopen($fileName, "a");
+	fwrite($fp, $data);
+	fclose($fp);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>映画の感想</title>
-</head>
-<body>
-<h1>映画の感想掲示板</h1>
-<form action="" method="post">
-タイトル:<input type="text" name="title">
-感想:<input type="text" name="impression">
-名前:<input type="text" name="name">
-<input type="submit" value="感想を投稿">
-</form>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>映画の感想</title>
+	</head>
+	<body>
+		<h1>映画の感想掲示板</h1>
+		<form action="" method="post">
+			タイトル:<input type="text" name="title">
+			感想:<input type="text" name="impression">
+			名前:<input type="text" name="name">
+			<input type="submit" value="感想を投稿">
+		</form>
+	</body>
 </html>
