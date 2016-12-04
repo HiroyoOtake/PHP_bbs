7675521ea917f1f0a97b203e41f9785d72671ede
otake <otake.hiroyo@gmail.com>
2016-12-05 00:36:52 +0900

    入力フォームの作成

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>映画の感想</title>
+</head>
+<body>
+<h1>映画の感想掲示板</h1>
+<form action="" method="post">
+タイトル:<input type="text" name="title">
+感想:<input type="text" name="impression">
+名前:<input type="text" name="name">
+<input type="submit" value="感想を投稿">
+</form>
+</body>
+</html>
